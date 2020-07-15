8867cc0ad5368b861c7aaf2ce9a04b0a18e755eb
MPerilly <msp501@nyu.edu>
2020-07-15 18:22:28 -0400

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/BAProg4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BAProg4.exe"
+  "BAProg4.exe.manifest"
+  "BAProg4.lib"
+  "BAProg4.pdb"
+  "CMakeFiles/BAProg4.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BAProg4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
