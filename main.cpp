0e2ca5d83c7ef031cea829a50e7e98848988136c
MPerilly <msp501@nyu.edu>
2020-07-15 18:47:18 -0400

    Brought in split function to parse input, began main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+vector<string> split(const string &str);
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // Parsing input:
+    string raw_inp;
+    cin >> raw_inp;
+    vector<string> split_inp = split(raw_inp);
+
+    const size_t num_nodes;
+    const size_t num_edges;
+
     return 0;
 }
 
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+    string::size_type start = 0;
+    string::size_type end = 0;
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+        start = end + 1;
+    }
+    tokens.push_back(str.substr(start));
+    return tokens;
+}
